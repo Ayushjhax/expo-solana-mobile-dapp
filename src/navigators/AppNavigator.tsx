009ad8029b777fc935ac022a8c0cde5bf045e3c3
ayushjhax <ayushkmrjha@yahoo.com>
2024-09-25 22:00:56 +0530

    chore: done

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -1,7 +1,3 @@
-/**
- * The app navigator (formerly "AppNavigator" and "MainNavigator") is used for the primary
- * navigation flows of your app.
- */
 import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
@@ -18,19 +14,6 @@ import {
   MD3LightTheme,
   adaptNavigationTheme,
 } from "react-native-paper";
-
-/**
- * This type allows TypeScript to know what routes are defined in this navigator
- * as well as what properties (if any) they might take when navigating to them.
- *
- * If no params are allowed, pass through `undefined`.
- *
- * For more information, see this documentation:
- *   https://reactnavigation.org/docs/params/
- *   https://reactnavigation.org/docs/typescript#type-checking-the-navigator
- *   https://reactnavigation.org/docs/typescript/#organizing-types
- *
- */
 
 type RootStackParamList = {
   Home: undefined;
@@ -61,14 +44,11 @@ declare global {
   }
 }
 
-
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
   }
 }
-
-// Documentation: https://reactnavigation.org/docs/stack-navigator/
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
